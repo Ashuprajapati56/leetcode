27a32bba5efe43374a51da4159a58494b1d8a0c1
Ashuprajapati56 <goleashu89@gmail.com>
2026-08-04 23:53:48 +0530

    Time: 0 ms (100%), Space: 15.6 MB (78.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int idx=-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                idx=i;
-                break;
+        int idx = -1;
 
+        // Step 1: Find pivot
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
             }
         }
-        if(idx==-1){
-            reverse(nums.begin(),nums.end());
+
+        // Step 2: If no pivot, reverse whole array
+        if (idx == -1) {
+            reverse(nums.begin(), nums.end());
             return;
         }
-        reverse(nums.begin()+(idx+1),nums.end());
-        int j=-1;
-        for(int i=idx+1;i<n;i++){
-            if(nums[i]>nums[idx]){
-                j=i;
+
+        // Step 3: Find just greater element from right
+        for (int i = n - 1; i > idx; i--) {
+            if (nums[i] > nums[idx]) {
+                swap(nums[i], nums[idx]);
                 break;
             }
         }
-        int temp=nums[idx];
-        nums[idx]=nums[j];
-        nums[j]=temp;
-        return;
+
+        // Step 4: Reverse suffix
+        reverse(nums.begin() + idx + 1, nums.end());
     }
 };
