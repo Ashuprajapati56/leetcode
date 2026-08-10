86eb69fd4a27e32fc9236b4f27a6d6e5a44ecb40
Ashuprajapati56 <goleashu89@gmail.com>
2026-08-10 22:51:39 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.02%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,11 +4,11 @@ public:
         int original = x;
         long long temp = 0;
 
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if (x <0) {
             return false;
         }
 
-        while (x > 0) {
+        while (x != 0) {
             int sum = x % 10;
             temp = temp * 10 + sum;
             x = x / 10;
