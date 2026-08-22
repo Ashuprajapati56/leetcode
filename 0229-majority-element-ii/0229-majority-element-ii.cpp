6c4f12249bc226e3972746af89c97042ec8880c0
Ashuprajapati56 <goleashu89@gmail.com>
2026-08-23 00:12:19 +0530

    Time: 7 ms (40.07%), Space: 26.5 MB (15.64%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
-        int el1 = 0, el2 = 0;
-        int cnt1 = 0, cnt2 = 0;
-
-        // Boyer-Moore Voting Algorithm
-        for (int i = 0; i < nums.size(); i++) {
-
-            if (cnt1 == 0 && nums[i] != el2) {
-                el1 = nums[i];
-                cnt1 = 1;
-            }
-            else if (cnt2 == 0 && nums[i] != el1) {
-                el2 = nums[i];
-                cnt2 = 1;
-            }
-            else if (nums[i] == el1) {
-                cnt1++;
-            }
-            else if (nums[i] == el2) {
-                cnt2++;
-            }
-            else {
-                cnt1--;
-                cnt2--;
-            }
-        }
-
-        // Verify the candidates
-        cnt1 = 0;
-        cnt2 = 0;
-
-        for (int num : nums) {
-            if (num == el1)
-                cnt1++;
-            else if (num == el2)
-                cnt2++;
-        }
-
+        
+        unordered_map<int, int> temp;
         vector<int> ans;
 
-        if (cnt1 > nums.size() / 3)
-            ans.push_back(el1);
+        // Frequency count
+        for (int num : nums) {
+            temp[num]++;
+        }
 
-        if (cnt2 > nums.size() / 3)
-            ans.push_back(el2);
+        // Check elements occurring more than n/3 times
+        for (auto it : temp) {
+            if (it.second > nums.size() / 3) {
+                ans.push_back(it.first);
+            }
+        }
 
         return ans;
     }
