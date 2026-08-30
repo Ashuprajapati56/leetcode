a6fcee903729e2467fc953a1db31b2673ec2efa7
Ashuprajapati56 <goleashu89@gmail.com>
2026-08-30 23:28:09 +0530

    Time: 0 ms (100%), Space: 52.2 MB (83.22%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -5,12 +5,15 @@ public:
 
         for(int i = 0; i < nums.size(); i++) {
 
+            // agar current index tak pahunch hi nahi sakte
             if(i > farthest) {
                 return false;
             }
 
+            // maximum reachable index update karo
             farthest = max(farthest, i + nums[i]);
 
+            // last index tak pahunch gaye
             if(farthest >= nums.size() - 1) {
                 return true;
             }
